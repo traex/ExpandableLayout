1821dcad3454010a5bac0b81a1d7c3344d661a76
Chutaux Robin <robin.chutaux@3ie.fr>
2014-09-15 21:19:43 +0200

    [INIT] Add project

---
+++ library/src/main/java/com/andexert/expandablelayout/library/ExpandableLayout.java
@@ -0,0 +1,157 @@
+/***********************************************************************************
+ * The MIT License (MIT)
+
+ * Copyright (c) 2014 Robin Chutaux
+
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ ***********************************************************************************/
+package com.andexert.expandablelayout.library;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.os.Handler;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
+import android.widget.RelativeLayout;
+
+import butterknife.ButterKnife;
+
+public class ExpandableLayout extends RelativeLayout
+{
+    private Boolean isAnimationRunning = false;
+    private Integer duration;
+
+    public ExpandableLayout(Context context)
+    {
+        super(context);
+    }
+
+    public ExpandableLayout(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    public ExpandableLayout(Context context, AttributeSet attrs, int defStyle)
+    {
+        super(context, attrs, defStyle);
+        init(context, attrs);
+    }
+
+    private void init(final Context context, AttributeSet attrs)
+    {
+        final View rootView = View.inflate(context, R.layout.view_expandable, this);
+        final RelativeLayout headerRelativeLayout = (RelativeLayout) rootView.findViewById(R.id.view_expandable_headerlayout);
+        final RelativeLayout contentRelativeLayout = (RelativeLayout) rootView.findViewById(R.id.view_expandable_contentLayout);
+        final TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.ExpandableLayout);
+        final int headerID = typedArray.getResourceId(R.styleable.ExpandableLayout_headerLayout, -1);
+        final int contentID = typedArray.getResourceId(R.styleable.ExpandableLayout_contentLayout, -1);
+        ButterKnife.inject(rootView);
+
+        if (headerID == -1 || contentID == -1)
+            throw new IllegalArgumentException("HeaderLayout and ContentLayout cannot be null!");
+
+        duration = typedArray.getInt(R.styleable.ExpandableLayout_duration, getContext().getResources().getInteger(android.R.integer.config_shortAnimTime));
+        final View headerView = View.inflate(context, headerID, null);
+        headerView.setLayoutParams(new ViewGroup.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+        headerRelativeLayout.addView(headerView);
+
+        final View contentView = View.inflate(context, contentID, null);
+        contentView.setLayoutParams(new ViewGroup.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+        contentRelativeLayout.addView(contentView);
+
+        contentRelativeLayout.setVisibility(GONE);
+        headerRelativeLayout.setOnClickListener(new OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                if (!isAnimationRunning)
+                {
+                    if (contentRelativeLayout.getVisibility() == VISIBLE)
+                        collapse(contentRelativeLayout);
+                    else
+                        expand(contentRelativeLayout);
+
+                    isAnimationRunning = true;
+                    new Handler().postDelayed(new Runnable()
+                    {
+                        @Override
+                        public void run()
+                        {
+                            isAnimationRunning = false;
+                        }
+                    }, duration);
+                }
+            }
+        });
+    }
+
+    private void expand(final View v)
+    {
+        v.measure(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+        final int targetHeight = v.getMeasuredHeight();
+        v.getLayoutParams().height = 0;
+        v.setVisibility(VISIBLE);
+        Animation animation = new Animation()
+        {
+            @Override
+            protected void applyTransformation(float interpolatedTime, Transformation t)
+            {
+                v.getLayoutParams().height = (interpolatedTime == 1) ? LayoutParams.WRAP_CONTENT : (int) (targetHeight * interpolatedTime);
+                v.requestLayout();
+            }
+
+            @Override
+            public boolean willChangeBounds() {
+                return true;
+            }
+        };
+        animation.setDuration(duration);
+        v.startAnimation(animation);
+    }
+
+    private void collapse(final View v)
+    {
+        final int initialHeight = v.getMeasuredHeight();
+        Animation animation = new Animation()
+        {
+            @Override
+            protected void applyTransformation(float interpolatedTime, Transformation t) {
+                if(interpolatedTime == 1){
+                    v.setVisibility(View.GONE);
+                }else{
+                    v.getLayoutParams().height = initialHeight - (int)(initialHeight * interpolatedTime);
+                    v.requestLayout();
+                }
+            }
+
+            @Override
+            public boolean willChangeBounds() {
+                return true;
+            }
+        };
+
+        animation.setDuration(duration);
+        v.startAnimation(animation);
+    }
+}
